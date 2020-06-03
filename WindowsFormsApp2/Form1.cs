923fe6a9f66136e1ed10c70e1c2791e617f5278a
ye eun <0hyeeun263@gmail.com>
2020-06-03 11:16:42 +0900

    초기 커밋.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            myButton.Text = "hi";
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("이곳의 문자열을 출력합니다");
+        }
+    }
+}
